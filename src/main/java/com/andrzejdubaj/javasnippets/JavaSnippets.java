f79188b54ad922d5578fb9e6eb591822884845cb
coffeina <dubaj.andrzej@gmail.com>
2022-12-31 02:05:45 +0100

    Cleanup

--- src/main/java/com/andrzejdubaj/javasnippets/JavaSnippets.java
+++ src/main/java/com/andrzejdubaj/javasnippets/JavaSnippets.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.andrzejdubaj.javasnippets;
 
 /**
  *
  * @author coffeina
  */
+
 public class JavaSnippets {
 
     public static void main(String[] args) {
