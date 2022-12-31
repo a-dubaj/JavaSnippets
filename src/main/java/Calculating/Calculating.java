5214565e2bf56b7dffbd44651fc780f7a7e5fc2a
coffeina <dubaj.andrzej@gmail.com>
2022-12-31 02:43:40 +0100

    Update

--- src/main/java/Calculating/Calculating.java
+++ src/main/java/Calculating/Calculating.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 /**
  *
- * @author coffeina
+ * @author Andrzej Dubaj
  */
-
 public class Calculating {
-    public static void main(String[] main){
+
+    public static void main(String[] main) {
         Scanner scanner = new Scanner(System.in);
-        
+
         int first = 1;
         int second = 2;
-       
+
         first = Integer.valueOf(scanner.nextLine());
         second = Integer.valueOf(scanner.nextLine());
-        
+
         int product = first * second;
         System.out.println("The product of " + first + " and " + second + " is " + product);
     }
