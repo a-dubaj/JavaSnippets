69b5335ff384693ec04508e340a2b714a39e7beb
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2023-01-01 21:55:40 +0100

    Update

---
+++ src/main/java/EndingLoop/Loop.java
@@ -0,0 +1,15 @@
+package EndingLoop;
+
+public class Loop {
+    public static void main(String[] main) {
+        int number = 1;
+        while (true) {
+            System.out.println(number);
+            if (number >= 5) {
+                break;
+            }
+            number = number + 1;
+        }
+        System.out.println("Ready!");
+    }
+}
--- src/main/java/Square/Square.java
+++ src/main/java/Square/Square.java
@@ -1,5 +1,4 @@
 package Square;
-
 import java.util.Scanner;
 
 public class Square {
