6379853d9c6fe688c3562deb0a4e0b804e8be9d8
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2023-01-01 13:04:01 +0100

    Square

---
+++ src/main/java/Square/Square.java
@@ -0,0 +1,15 @@
+package Square;
+
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] main) {
+        Scanner scanner = new Scanner(System.in);
+        int valueOne = scanner.nextInt();
+        int valueTwo = scanner.nextInt();
+        int sum = valueTwo + valueOne;
+
+        double square = Math.sqrt(sum);
+        System.out.println(square);
+    }
+}
